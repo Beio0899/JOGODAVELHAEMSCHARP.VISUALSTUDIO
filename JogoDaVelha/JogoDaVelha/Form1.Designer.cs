b85cfa455cee671c9dfa51e7997fe10d96295e30
Beio0899 <goncalvesfragosoezequiel@gmail.com>
2024-10-09 21:40:11 -0300

    Add files via upload

---
+++ JogoDaVelha/JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(2, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 134);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(167, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 134);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(332, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 134);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(2, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 134);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(167, 141);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 134);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Location = new System.Drawing.Point(332, 141);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(159, 134);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Location = new System.Drawing.Point(2, 281);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(159, 134);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.Location = new System.Drawing.Point(167, 281);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(159, 134);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.Location = new System.Drawing.Point(332, 281);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(159, 134);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReiniciar.Location = new System.Drawing.Point(552, 68);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(114, 52);
+            this.btnReiniciar.TabIndex = 9;
+            this.btnReiniciar.Text = "Iniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Location = new System.Drawing.Point(548, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button10.Location = new System.Drawing.Point(672, 68);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(114, 52);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Limpar";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Location = new System.Drawing.Point(496, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 417);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JOGO DA VELHA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ JogoDaVelha/JogoDaVelha/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        char[,] tabuleiro = new char[3, 3];
+        char jogadorAtual = 'X';
+        bool jogoEmAndamento = true;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[0, 0] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[0, 0] = jogadorAtual;
+                button1.Text = jogadorAtual.ToString();
+
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+                MarcarJogada(sender as Button);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[0, 1] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[0, 1] = jogadorAtual;
+                button2.Text = jogadorAtual.ToString();
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[0, 2] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[0, 2] = jogadorAtual;
+                button3.Text = jogadorAtual.ToString();
+
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[1, 0] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[1, 0] = jogadorAtual;
+                button4.Text = jogadorAtual.ToString();
+
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[1, 1] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[1, 1] = jogadorAtual;
+                button5.Text = jogadorAtual.ToString();
+
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[1, 2] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[1, 2] = jogadorAtual;
+                button6.Text = jogadorAtual.ToString();
+
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[2, 0] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[2, 0] = jogadorAtual;
+                button7.Text = jogadorAtual.ToString();
+
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+            }
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[2, 1] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[2, 1] = jogadorAtual;
+                button8.Text = jogadorAtual.ToString();
+
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            if (tabuleiro[2, 2] == ' ' && jogoEmAndamento)
+            {
+                tabuleiro[2, 2] = jogadorAtual;
+                button9.Text = jogadorAtual.ToString();
+
+                char vencedor = VerificarVitoria();
+                if (vencedor != ' ')
+                {
+                    label1.Text = $"Jogador {vencedor} venceu!";
+                    jogoEmAndamento = false;
+                }
+                else if (VerificarEmpate())
+                {
+                    label1.Text = "Empate!";
+                    jogoEmAndamento = false;
+                }
+                else
+                {
+                    jogadorAtual = jogadorAtual == 'X' ? 'O' : 'X';
+                }
+                AtualizarInterface();
+            }
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ReiniciarJogo();
+            label2.Text = " Pode fazer a primeira jogada";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        char VerificarVitoria()
+        {
+            // Verificar linhas, colunas e diagonais
+            for (int i = 0; i < 3; i++)
+            {
+                if (tabuleiro[i, 0] == tabuleiro[i, 1] && tabuleiro[i, 1] == tabuleiro[i, 2] && tabuleiro[i, 0] != ' ')
+                    return tabuleiro[i, 0];
+                if (tabuleiro[0, i] == tabuleiro[1, i] && tabuleiro[1, i] == tabuleiro[2, i] && tabuleiro[0, i] != ' ')
+                    return tabuleiro[0, i];
+            }
+            if (tabuleiro[0, 0] == tabuleiro[1, 1] && tabuleiro[1, 1] == tabuleiro[2, 2] && tabuleiro[0, 0] != ' ')
+                return tabuleiro[0, 0];
+            if (tabuleiro[0, 2] == tabuleiro[1, 1] && tabuleiro[1, 1] == tabuleiro[2, 0] && tabuleiro[0, 2] != ' ')
+                return tabuleiro[0, 2];
+            return ' ';
+        }
+        private void ReiniciarJogo()
+        {
+            // Limpar tabuleiro, label e resetar variáveis
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    tabuleiro[i, j] = ' ';
+                }
+            }
+
+            // Limpar os textos dos botões
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+            label1.Text
+ = "";
+            jogadorAtual = 'X';
+            jogoEmAndamento = true;
+        }
+
+        private bool VerificarEmpate()
+        {
+            // Verifica se todas as posições do tabuleiro estão ocupadas
+            return tabuleiro.Cast<char>().All(c => c != ' ');
+        }
+        private void MarcarJogada(Button botao)
+        {
+            if (botao.Text != "" || !jogoEmAndamento)
+            {
+                return; // Botão já está marcado ou jogo encerrado
+            }
+
+            int linha = int.Parse(botao.Name.Substring(6, 1)) - 1; // Obtém a linha do botão
+            int coluna = int.Parse(botao.Name.Substring(7, 1)) - 1; // Obtém a coluna do botão
+
+            tabuleiro[linha, coluna] = jogadorAtual;
+            botao.Text = jogadorAtual.ToString();
+
+            // ... (restante do código)
+        }
+        private bool VerificarLinha(int linha)
+        {
+            // Verifica se uma linha está completa
+            return tabuleiro[linha, 0] == tabuleiro[linha, 1] && tabuleiro[linha, 1] == tabuleiro[linha, 2] && tabuleiro[linha, 0] != ' ';
+        }
+
+        // Funções similares para verificar coluna e diagonais
+
+
+
+        private void AtualizarInterface()
+        {
+            // Atualiza os textos dos botões de acordo com o tabuleiro
+            button1.Text = tabuleiro[0, 0].ToString();
+            // ... (atualizar os outros botões)
+        }
+               private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            ReiniciarJogo();
+            label1.Text = "";
+       
+
+        }
+    }
+}
